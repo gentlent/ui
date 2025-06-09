6a14328b6205a0e2ae07a45d4fabaa9e01965071
Tom Klein <tom@gentlent.com>
2025-06-09 15:24:57 +0200

    Update Cards

--- components/card/Card.js
+++ components/card/Card.js
@@ -7,9 +7,16 @@ const StyledCard = styled.div`
   border: 1px solid ${(props) => props.theme.borderColor};
   border-radius: ${(props) => props.theme.baseBorderRadius}px;
   padding: ${(props) => !props.noPadding && props.theme.baseSpacingSize * 4}px;
-  box-shadow: ${boxShadow.medium};
   margin: ${(props) => props.theme.baseSpacingSize * 3.333}px 0;
   overflow: hidden;
+
+  ${(props) => props.type === 'default' && `
+    box-shadow: ${boxShadow.medium};
+  `}
+
+  ${(props) => props.type === 'flat' && `
+    background: ${props.theme.borderColor};
+  `}
 
   ${(props) => !props.noPadding && `
     & > *:last-child {
@@ -29,8 +36,10 @@ const StyledCard = styled.div`
 `;
 
 export default function Card(props) {
+  const type = props.type || 'default';
+
   const { theme } = useContext(ThemeContext);
-  return <StyledCard theme={theme} {...props}>
+  return <StyledCard theme={theme} {...props} type={type}>
     {props.children}
   </StyledCard>;
 }
